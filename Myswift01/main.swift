81812007cdfecc221781db8c6f9eb3c65eefc0a3
Prince <richard0629823@hotmail.com>
2017-09-06 10:47:59 +0800

    Initial Commit

---
+++ Myswift01/main.swift
@@ -0,0 +1,22 @@
+//
+//  main.swift
+//  Myswift01
+//
+//  Created by user21 on 2017/9/6.
+//
+//
+
+import Foundation
+
+print("Hello, World!")
+
+var Rich1:Int=12
+var Rich2:Double=12.3
+var Rich3:Int=Rich1+Int(Rich2)
+print(Rich3)
+
+
+//var a = Int.max
+//print(a)
+//var b = a-1
+//print(b)
